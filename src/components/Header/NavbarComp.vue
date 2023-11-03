52120f4f546faf537f7286847b7b37ac1aaf265f
bw007 <abdulaziz00791@gmail.com>
2023-11-04 00:56:43 +0500

    Full navbar component

--- src/components/Header/NavbarComp.vue
+++ src/components/Header/NavbarComp.vue
@@ -7,28 +7,33 @@
     </div>
     <form @submit.prevent class="w-3/5">
       <div class="relative bg-violet-50 rounded">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-5 pointer-events-none">
           <img src="/search.svg">
         </div>
         <input type="search"
-          class="block w-full pl-12 pr-6 py-2.5 outline-none text-lg text-gray-900 rounded bg-transparent border border-transparent focus:border-blue-200"
+          class="block w-full pl-14 pr-6 py-2.5 outline-none text-lg text-gray-900 rounded bg-transparent border border-transparent focus:border-blue-200"
           placeholder="Search">
       </div>
     </form>
     <div class="flex items-center justify-between w-44">
-      <div class="flex items-center justify-center w-12 h-12 cursor-pointer bg-violet-50 rounded">
+      <div @click="userToggle = !userToggle" class="relative flex items-center justify-center p-3 cursor-pointer bg-violet-50 active:bg-violet-100 rounded">
         <img class="opacity-70" src="/avatar-out.svg" alt="">
+        <router-link to="/login">
+          <span v-if="userToggle" class="w-20 text-sm text-center absolute top-14 right-6 bg-slate-50 hover:bg-slate-100 active:bg-violet-100 py-2 px-5 rounded">Log in</span>
+        </router-link>
       </div>
-      <div class="flex items-center justify-center w-12 h-12 cursor-pointer bg-violet-50 rounded">
+      <div class="flex items-center justify-center p-3 cursor-pointer bg-violet-50 active:bg-violet-100 rounded">
         <img class="opacity-70" src="/heart.svg" alt="">
       </div>
-      <div class="flex items-center justify-center w-12 h-12 cursor-pointer bg-violet-50 rounded">
-        <img class="opacity-70" src="/avatar-out.svg" alt="">
+      <div class="flex items-center justify-center p-3 cursor-pointer bg-violet-50 active:bg-violet-100 rounded">
+        <img class="opacity-70" src="/cart.svg" alt="">
       </div>
     </div>
   </nav>
 </template>
 
 <script setup>
-
+  import { ref } from "vue";
+  let userToggle = ref(false);
+  
 </script>
