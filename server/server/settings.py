767f29604de7364eafc9c6abdec8e1a3526b6aef
Husniddin Ravshanov <ravshanovhusniddin.2006@gmail.com>
2023-11-10 22:32:50 +0500

    Fixed the CORS headers bug

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,8 +39,8 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
 }
 
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8000']
-CSRF_ALLOWED_ORIGINS = ['http://127.0.0.1:8000']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5173']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
