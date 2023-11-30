f73d4273849a26d22baff8462480d492eb63c06e
Husniddin Ravshanov <ravshanovhusniddin.2006@gmail.com>
2023-11-30 17:31:40 +0500

    Fixed Book fields

--- server/library/admin.py
+++ server/library/admin.py
@@ -4,12 +4,12 @@ from .models import Author, Book, Rating
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name',)
-    search_fields = ('first_name', 'last_name',)
+    list_display = ('full_name',)
+    search_fields = ('full_name',)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date', 'copies_sold',)
+    list_display = ('title', 'author',  'copies_sold',)
     list_filter = ('author',)
     search_fields = ('title',)
     autocomplete_fields = ('author',)
--- server/library/models.py
+++ server/library/models.py
@@ -15,25 +15,24 @@ from datetime import datetime
 from account.models import User
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return self.full_name
 
 
 class Book(models.Model):
     author = models.ForeignKey(Author, models.CASCADE, related_name="books")
     title = models.CharField(max_length=255)
     # slug = AutoSlugField(populate_from=['title', 'id'])
-    description = models.TextField(max_length=255)
+    description = models.TextField(max_length=500)
     genre = models.CharField(max_length=50)
     length = models.IntegerField()
-    published_date = models.DateField()
-    created_date = models.DateField(default=datetime.now().today)
-    copies_sold = models.IntegerField(default=0)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    discount = models.IntegerField(default=0, validators=[MaxValueValidator(100)])
+    published_date = models.IntegerField()
+    created_date = models.DateField(default=datetime.now().today, blank=True)
+    copies_sold = models.IntegerField(default=0, blank=True)
+    price = models.IntegerField()
+    discount = models.IntegerField(default=0, validators=[MaxValueValidator(100)], blank=True)
     
 
     cover = models.ImageField(upload_to='books/covers')
