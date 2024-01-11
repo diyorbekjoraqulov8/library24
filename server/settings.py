3b2c77dd3fa22a44aed9bb52fd347a34b71086ef
Husniddin Ravshanov <ravshanovhusniddin.2006@gmail.com>
2024-01-11 20:02:42 +0500

    Disabled DRF render template

--- server/settings.py
+++ server/settings.py
@@ -34,7 +34,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 SIMPLE_JWT = {
