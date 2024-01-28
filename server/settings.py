068074818bd9877c4ee4660c7f953123e56846d5
Husniddin Ravshanov <ravshanovhusniddin.2006@gmail.com>
2024-01-28 12:28:24 +0500

    Added documentation templates

--- server/settings.py
+++ server/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'django_filters',
+    'drf_yasg',
 
 
     'account',
--- server/urls.py
+++ server/urls.py
@@ -2,9 +2,25 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library24 API",
+      default_version='v1',
+      description="API Documentation for Library24",
+      contact=openapi.Contact(email="ravshanovhusniddin.2006@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('account.urls')),
     path('library/', include('library.urls')),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
